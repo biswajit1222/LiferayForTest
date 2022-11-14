aaf21938a16f897063242b9664dba4de98f8dcf6
biswajit.nayak01 <biswajit.naya@192.168.0.101>
2022-11-14 16:53:46 +0530

    second commit

--- liferay/Liferay7.4CE/modules/Student_Master/src/main/java/Student_Master/ActionCommand/StudentAdd.java
+++ liferay/Liferay7.4CE/modules/Student_Master/src/main/java/Student_Master/ActionCommand/StudentAdd.java
@@ -1,9 +1,12 @@
 
 package Student_Master.ActionCommand;
+
 import java.util.List;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
+import javax.servlet.http.HttpServletRequest;
+
 import org.osgi.service.component.annotations.Component;
 import com.liferay.counter.kernel.service.CounterLocalServiceUtil;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
@@ -11,10 +14,13 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
 
 import StudentMasterService.model.Student;
 import StudentMasterService.service.StudentLocalServiceUtil;
 import Student_Master.constants.Student_MasterPortletKeys;
+import dbservice.service.studentLocalServiceUtil;
 
 @Component(immediate = true, property = { "javax.portlet.name=" + Student_MasterPortletKeys.STUDENT_MASTER,
 		"mvc.command.name=addStudent" }, service = MVCActionCommand.class)
@@ -23,6 +29,10 @@ public class StudentAdd extends BaseMVCActionCommand {
 	@SuppressWarnings("deprecation")
 	protected void doProcessAction(ActionRequest actionRequest, ActionResponse actionResponse) throws Exception {
 		_log.info("inside app::::::");
+
+		//_log.info("Student_MasterPortletKeys:::" + xx);
+		String xx1=ParamUtil.getString(actionRequest,"firstName");
+		_log.info("Student_MasterPortletKeys:::" + xx1);
 		String fName = actionRequest.getParameter("firstName");
 		_log.info("Student firstname:::" + fName);
 		String LName = actionRequest.getParameter("lastName");
@@ -34,18 +44,19 @@ public class StudentAdd extends BaseMVCActionCommand {
 		_log.info("Student city:::::" + city);
 		long studentId = CounterLocalServiceUtil.increment(Student.class.getName());
 		_log.info("Student id id:::::" + studentId);
-	Student student=StudentLocalServiceUtil.createStudent(studentId);
+		Student student = StudentLocalServiceUtil.createStudent(studentId);
 		student.setStudentId(studentId);
 		_log.info("Student added Sucessfully:::::" + studentId);
 		student.setFirstName(fName);
 		student.setLastName(LName);
 		student.setContactNo(phNo);
 		student.setWorkLocation(city);
-		String location="odisha";
-		List<Student> student1 =StudentLocalServiceUtil.getDataByWorkLocation(location);
+		String location = "odisha";
+		List<Student> student1 = StudentLocalServiceUtil.getDataByWorkLocation(location);
 		JSONFactoryUtil.createJSONObject();
-		System.out.println("city city city city::::::::::::::::::::::"+student1);
-		//StudentLocalServiceUtil.addStudent(student);
+		studentLocalServiceUtil.
+		System.out.println("city city city city::::::::::::::::::::::" + student1);
+		 StudentLocalServiceUtil.addStudent(student);
 		_log.info("Student added Sucessfully:::::" + student);
 	}
 }
